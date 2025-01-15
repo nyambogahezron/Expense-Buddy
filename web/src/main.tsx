43f1af141c87b5b68e8ec06a5611069286d84224
nyambogahezron <hezzyn1@gmail.com>
2025-01-15 20:40:37 +0300

    refactor main.tsx to simplify rendering logic and improve readability

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,17 +4,16 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import UserAgreement from './pages/user-agreement.tsx';
 
-const root = document.getElementById('root');
+const router = (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/user-agreement' element={<UserAgreement />} />
+    </Routes>
+  </BrowserRouter>
+);
 
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/user-agreement' element={<UserAgreement />} />
-      </Routes>
-    </BrowserRouter>
-  );
-} else {
-  console.error('Failed to find the root element');
-}
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement!);
+
+root.render(router);
