9f7dadc097d3ca84cca20d9c3eec9491ba1a5a96
nyambogahezron <hezzyn1@gmail.com>
2025-01-23 22:50:12 +0300

    add LandingPage component with sections, update global styles, and include new dependencies

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
   ],
   presets: [require('nativewind/preset')],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       fontFamily: {
         pthin: ['Poppins-Thin', 'sans-serif'],
@@ -26,6 +32,14 @@ module.exports = {
         bgPrimaryLight: '#f3f4f6',
         orangeClr: '#F38D1B',
         orangeClrLight: '#FF7F50',
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
       },
     },
   },
